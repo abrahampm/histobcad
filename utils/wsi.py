6356063c0f95901b3d01be577f3d6fb38973f17b
Carlos Abraham Pérez Marrero <abrahampm96@gmail.com>
2021-09-19 18:35:05 -0400

    Create wsi utils script

--- utils/wsi.py
+++ utils/wsi.py
@@ -1,10 +1,10 @@
-from skimage import io
-from numpy import zeros
+from numpy import zeros, ndarray
+from matplotlib.pyplot import cm
+from skimage.filters import gaussian
 
 
-def split_wsi(filename, tile_width=50, tile_height=50):
-    wsi = io.imread(filename)
-    wsi_height, wsi_width = wsi.shape[0], wsi.shape[1]
+def split_image(image: ndarray, tile_width=50, tile_height=50) -> ndarray:
+    wsi_height, wsi_width = image.shape[0], image.shape[1]
 
     # Create tiles[x,y] array discarding tiles with width < TILE_WIDTH / 2 or height < TILE_HEIGHT / 2_
     tiles = zeros([round(wsi_width / tile_width), round(wsi_height / tile_height), tile_width, tile_height, 3], dtype='uint8')
@@ -23,8 +23,18 @@ def split_wsi(filename, tile_width=50, tile_height=50):
             if y1 > wsi_height:
                 y1 = wsi_height
                 j1 = y1 - y
-            tiles[i, j, 0:j1, 0:i1, 0:3] = wsi[y:y1, x:x1, 0:3]
+            tiles[i, j, 0:j1, 0:i1, 0:3] = image[y:y1, x:x1, 0:3]
             j += 1
         i += 1
 
-    return wsi, tiles
+    return tiles
+
+
+def create_heatmap(mask: ndarray, sigma=15):
+    mask = gaussian(mask, sigma=sigma)
+    # TODO Use another colormap to exclude matplotlib from dependencies
+    cmap = cm.get_cmap('jet')
+    rgba_mask = cmap(mask)
+    rgba_mask[..., -1] = mask
+    rgba_mask *= 255
+    return rgba_mask.astype('uint8')
