703bcf6c7377c9fb693b849a15cec189c42c4043
Carlos Abraham Pérez Marrero <abrahampm96@gmail.com>
2022-10-31 12:36:47 -0400

    Add bash script to fix conda environment c++ libraries

---
+++ fix-conda-env.sh
@@ -0,0 +1,8 @@
+export USER=abraham
+export ENV=HistoBCAD
+cd /home/$USER/miniconda3/envs/$ENV/lib || exit
+mkdir backup  # Create a new folder to keep the original libstdc++
+mv libstd* backup  # Put all libstdc++ files into the folder, including soft links
+cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6  ./ # Copy the c++ dynamic link library of the system here
+ln -s libstdc++.so.6 libstdc++.so
+ln -s libstdc++.so.6 libstdc++.so.6.0.19
