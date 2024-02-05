24d9b26838d4df87406a01b0cc42fff75e63fa9b
Carlos Abraham Pérez Marrero <abrahampm96@gmail.com>
2024-02-04 20:42:57 -0500

    Revert "Create base Viewer class"
    This reverts commit 94deb08612478a37a75851b4721efacddca8f586.

--- library/deepzoom/deepzoom_viewer.py
+++ library/deepzoom/deepzoom_viewer.py
@@ -1,12 +1,21 @@
-from PySide2.QtCore import Signal, Property, Slot
+import os
+import re
 
-from library.base.viewer import Viewer
+from PySide2.QtCore import QObject, QStringListModel, QUrl, Signal, Property, Slot, QAbstractItemModel
+
 from library.deepzoom.deepzoom_server import DeepZoomServer
 
 
-class DeepZoomViewer(Viewer):
+class DeepZoomViewer(QObject):
     def __init__(self, server: DeepZoomServer):
-        super().__init__()
+        QObject.__init__(self)
+        self._selected_file_url = ""
+        self._selected_file_name = ""
+        self._selected_file_folder = ""
+        self._selected_file_thumbnail_url = ""
+        self._last_selected_file_folder = ""
+        self._selected_file_siblings = QStringListModel(self)
+        self._supported_file_extensions = ""
         self._server = server
         self._dzi_levels = tuple()
         self._dzi_dimensions = tuple()
@@ -14,16 +23,32 @@ class DeepZoomViewer(Viewer):
         self._dzi_max_zoom_level = 0
         self.set_supported_file_extensions()
 
+    def get_selected_file(self):
+        return self._selected_file_url
+
+    def get_selected_file_siblings(self):
+        return self._selected_file_siblings
+
     def set_selected_file(self, file):
-        self._set_selected_file(file)
+        if isinstance(file, QUrl):
+            if file.isLocalFile():
+                self._selected_file_path = file.toLocalFile()
+            else:
+                file_name = file.toString().split('/')[3]
+                self._selected_file_path = os.path.join(self._selected_file_folder, file_name)
+        elif isinstance(file, str):
+            self._selected_file_path = file
+        else:
+            return
+        self._selected_file_name = os.path.basename(self._selected_file_path)
+        self._selected_file_folder = os.path.dirname(self._selected_file_path)
         self._server.set_base_dir(self._selected_file_folder)
         self._set_dzi_info()
         self._selected_file_url = self._server.get_slide_url(self._selected_file_name)
         self.on_selected_file.emit()
         self.reload.emit()
         self.set_selected_file_thumbnail()
-        siblings = [self._server.get_thumbnail_url(a) for a in self._scan_working_dir()]
-        self.set_selected_file_siblings(siblings)
+        self._detect_selected_file_siblings()
         print("Selected file: ", self._selected_file_url)
         print("Selected file folder: ", self._selected_file_folder)
 
@@ -31,6 +56,19 @@ class DeepZoomViewer(Viewer):
     def set_selected_file_siblings(self, files):
         self._selected_file_siblings.setStringList(files)
         self.on_selected_file_siblings.emit()
+
+    def _detect_selected_file_siblings(self):
+        if self._selected_file_folder != self._last_selected_file_folder:
+            self._last_selected_file_folder = self._selected_file_folder
+            temp_siblings = []
+            with os.scandir(self._selected_file_folder) as d:
+                for entry in d:
+                    supported_file_extensions_re = re.compile('.' + '$|.'.join(self._supported_file_extensions) + '$')
+                    if entry.is_file() and re.search(supported_file_extensions_re, entry.name):
+                        slide_path = self._server.get_thumbnail_url(entry.name)
+                        temp_siblings.append(slide_path)
+            temp_siblings = sorted(temp_siblings)
+            self.set_selected_file_siblings(temp_siblings)
 
     def _set_dzi_info(self):
         self._dzi_levels = self._server.get_level_tiles(self._selected_file_name)
@@ -77,14 +115,25 @@ class DeepZoomViewer(Viewer):
     def get_tile_size(self):
         return self._server.get_tile_dimensions()[0]
 
+    reload = Signal()
     on_tile_size = Signal()
     on_dzi_max_width = Signal()
     on_dzi_max_height = Signal()
     on_dzi_min_zoom_level = Signal()
     on_dzi_max_zoom_level = Signal()
 
+    on_selected_file = Signal()
+    on_selected_file_thumbnail = Signal()
+    on_selected_file_siblings = Signal()
+    on_supported_file_extensions = Signal()
+
     dzi_tile_size = Property(int, get_tile_size, notify=on_tile_size)
     dzi_max_width = Property(int, get_dzi_max_width, notify=on_dzi_max_width)
     dzi_max_height = Property(int, get_dzi_max_height, notify=on_dzi_max_height)
     dzi_min_zoom_level = Property(int, get_dzi_min_zoom_level, set_dzi_min_zoom_level, notify=on_dzi_min_zoom_level)
     dzi_max_zoom_level = Property(int, get_dzi_max_zoom_level, set_dzi_max_zoom_level, notify=on_dzi_max_zoom_level)
+
+    selected_file = Property(QUrl, get_selected_file, set_selected_file, notify=on_selected_file)
+    selected_file_thumbnail = Property(QUrl, get_selected_file_thumbnail, set_selected_file_thumbnail, notify=on_selected_file_thumbnail)
+    selected_file_siblings = Property(QAbstractItemModel, get_selected_file_siblings, set_selected_file_siblings, notify=on_selected_file_siblings)
+    supported_file_extensions = Property(str, get_supported_file_extensions, notify=on_supported_file_extensions)
